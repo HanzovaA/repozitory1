5243f60ce78d750e1f626491e79805be5d1f8165
HanzovaA <ajahanzova@seznam.cz>
2023-02-22 22:20:05 +0100

    commit

---
+++ SQL/console.sql
@@ -0,0 +1,13 @@
+CREATE TABLE USERS (
+    UserID int,
+    Name varchar(255),
+    age int,
+    created DATE,
+    created_by varchar(255)
+);
+
+INSERT INTO USERS values (1,'Petr Tichý',23,'2023-02-22','Alena');
+INSERT INTO USERS VALUES (2,'Jan Novák',36,'2023-02-22','Alena') ;
+INSERT INTO USERS VALUES (3,'Vojtěch Kostka',41,'2023-02-22','Alena') ;
+INSERT INTO USERS VALUES (4,'Jakub Hořejší',33,'2023-02-22','Alena') ;
+INSERT INTO USERS VALUES (5,'Ondřej Žáček',45,'2023-02-22','Alena') ;
